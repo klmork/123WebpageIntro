87968879230f9b98d5ca47910fe5e612f18c30c4
klmork <klmork@calpoly.edu>
2023-10-15 22:36:45 -0700

    task 3 demo

--- src/sketch.js
+++ src/sketch.js
@@ -1,7 +1,14 @@
+let x = 0;
 function setup() {
   createCanvas(400, 400);
 }
 
 function draw() {
   background(220);
+  ellipse(x, 200, 50);
+  x = x + 1;
+  if (x > width / 2) {
+    document.querySelector("body").style.backgroundColor = "green";
+    document.querySelector("h1").style.color = "red";
+  }
 }
